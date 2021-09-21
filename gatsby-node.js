3f7931be5e7a76c2bb30bf1d9bbf468c9200e871
sasha <alexander.aleshchenko@mail.mcgill.ca>
2021-09-21 03:43:12 -0400

    Commit to force rebuild of site

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@
 //  * See: https://www.gatsbyjs.com/docs/node-apis/
 //  */
 
-// // You can delete this file if you're not using it
+// // You can  this file if you're not using it
 
 // // gatsby-node.js
 // const axios = require('axios');
